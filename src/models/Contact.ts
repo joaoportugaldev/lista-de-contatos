c294f2b172024e4f178c5aeaa79c22249c582ffe
João Portugal <joaosportugal@hotmail.com>
2024-12-30 18:24:10 -0300

    fixed: something about prettier

--- src/models/Contact.ts
+++ src/models/Contact.ts
@@ -13,22 +13,3 @@ class Contact {
 }
 
 export default Contact
-
-export const joao = new Contact(
-  'João Portugal',
-  '21989434454',
-  'joaosportugal@hotmail.com',
-  1
-)
-export const pedro = new Contact(
-  'Pedro Menescal',
-  '71988874729',
-  'castelobranco@gmail.com',
-  2
-)
-export const bruno = new Contact(
-  'Bruno Reis',
-  '61787878787',
-  'brunoreis@outlook.com',
-  3
-)
